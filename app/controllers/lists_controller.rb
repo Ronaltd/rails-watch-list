dc6db795654e118aff7bace2e0c7a972dbcbd40f
Ronalt Batista <ronaltdbatista@gmail.com>
2021-05-31 18:57:25 -0300

    codigo parcial

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,18 +1,22 @@
 class ListsController < ApplicationController
   def index
-    @list = List.all
+    @lists = List.all
   end
 
   def show
-    
+    @bookmark = Bookmark.new
   end
 
   def new
-    
+    @list = List.new
   end
 
   def create
-    
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
   end
 
 end
